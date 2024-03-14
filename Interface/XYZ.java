b68a81b6e7b0252441a1734d0a016fc24f89131e
VedantGadge <137107043+VedantGadge@users.noreply.github.com>
2024-03-15 01:14:18 +0530

    Update XYZ.java

--- Interface/XYZ.java
+++ Interface/XYZ.java
@@ -17,7 +17,7 @@ obj.method1();
 
 /*
  We cannot create object inside an abstract classes and interfaces.
- So we make an object of an abstract class or an interface(obv outseide them), we use 
- the objects of it's child class as a refernce object.So this is called as Dynamic method dispatch.
+ So we make an object of an abstract class or an interface(obv outside them), we use 
+ the objects of it's child class as a reference object.So this is called as Dynamic method dispatch.
  This is an application of dynamic polymorphism.
  */
