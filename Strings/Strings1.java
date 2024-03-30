e0b34232e171262f0ff14bd5406dfddb516a155f
Vedant Gadge <vedant.gadgegsis@gmail.com>
2024-03-30 18:58:03 +0530

    deleted:    Area.class 	deleted:    Area_MOR.class 	deleted:    Area_MOR.java 	deleted:    Area_MOR.txt 	deleted:    Array_Sum.java renamed:    Strings3.class -> Strings/Strings3.class 	renamed:    Strings3.java -> Strings/Strings3.java 	renamed:    Strings4.class -> Strings/Strings4.class 	renamed:    Strings4.java -> Strings/Strings4.java 	renamed:    Strings5.class -> Strings/Strings5.class 	renamed:    Strings5.java -> Strings/Strings5.java 	deleted:    Student.class

---
+++ Strings/Strings1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Strings1
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+String s;
+System.out.println("Enter the string: ");
+s = sc.nextLine();
+System.out.println("The entered string is: "+s);
+}
+}
