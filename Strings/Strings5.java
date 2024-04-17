bd9d07673243a87e1e56bb98467f292a7a26e986
VedantGadge <vedant.gadgegsis@gmail.com>
2024-04-17 17:33:57 +0530

    Changes to be committed: 	modified:   Strings/Strings5.class 	modified:   Strings/Strings5.java  Changes not staged for commit: 	modified:   MultiThreading/Stopwatch.class 	modified:   MultiThreading/Stopwatch.java 	modified:   MultiThreading/endStopWatch.class 	modified:   MultiThreading/printStopWatch.class 	MultiThreading/InputHandler.class 	MultiThreading/Stopwatch1.class 	MultiThreading/Stopwatch1.java 	MultiThreading/startStopWatch.class

--- Strings/Strings5.java
+++ Strings/Strings5.java
@@ -3,19 +3,20 @@ class Strings5
 {
 public static void main(String Args[])
 {
-String s,temp;
-s = temp;
+String s;
+int k=0;
 Scanner sc = new Scanner(System.in);
 System.out.println("Enter String: ");
 s = sc.nextLine();
 for(int i = 0;i<=(s.length()/2);i++)
 {
-s.charAt(i)=sc.charAt(s.length()-i);
+if(s.charAt(i)!=s.charAt(s.length()-1-i))
+    k=1;
 }
-if(s==temp)
-System.out.println("Palindrome!!");
-else
+if(k==1)
 System.out.println("Not Palindrome!!");
+else
+System.out.println("Palindrome!!");
 }
 }
 
@@ -23,4 +24,4 @@ System.out.println("Not Palindrome!!");
 
 
 
-
+
